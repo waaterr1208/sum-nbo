a584571561f4df748e5e6d8818fadae7c64346d0
waaterr1208 <waaterr0516@gmail.com>
2025-07-16 00:03:13 +0900

    Refactor sum-nbo.cpp

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ all: $(TARGET)
 
 $(TARGEt): sum-nbo.o
 	$(LINK.cpp) $^ $(LOADLIBES) $(LDLIBS) -o $@
+
+clean:
+	rm -f *.o
+	rm -f *.bin
--- sum-nbo.cpp
+++ sum-nbo.cpp
@@ -1,38 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <string.h>
 #include <netinet/in.h>
-
-uint32_t trans(FILE* fp) {
-	uint32_t n;
-	size_t read = fread(&n, 1, sizeof(n), fp);
-	if(read != sizeof(n)) {
-		printf("파일이 4바이트가 아닙니다");
-		return -1;
-	}
-	return ntohl(n);
-}
 
 int main(int argc, char* argv[]) {
 	
 	uint32_t res = 0;
 
 	for (int i = 1; i<argc; i++) {
+		uint32_t n;
 		FILE* fp = fopen(argv[i], "r");
-		
-		if(i != 1) {
-			printf(" + ");
-		}
-
 		if(fp == NULL) {
-			printf("파일 입력 에러");
+			printf("\n %s: 파일 입력 에러", argv[i]);
+			return -1;
+		}
+	
+		size_t read = fread(&n, 1, sizeof(n), fp);
+		if(read < sizeof(n)){
+			printf("\n %s: 파일이 4바이트보다 작습니다.", argv[i]);
 			return -1;
 		}
 		
-		uint32_t num = trans(fp);
-		printf("%d(0x%08x)", num, num);
+		n = ntohl(n);
 
-		res += num;
+		if(i != 1) {
+			printf(" + ");
+		}	
+		printf("%d(0x%08x)", n, n);
+
+		res += n;
 		fclose(fp);
 	}
 
